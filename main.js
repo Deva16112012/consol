74ce5ae9c47393dbdeb6cdc592279027a0a7262f
Deva16112012 <100427772+Deva16112012@users.noreply.github.com>
2022-02-25 20:39:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("Welcome to do console");
+var a="Deva";
+var b="Nanda";
+var c=a.concat(b);
+console.log("first name is",a);
+console.log("second name is",b);
+console.log("Full name is",c);
+var d=c.toLowerCase();
+console.log(d);
